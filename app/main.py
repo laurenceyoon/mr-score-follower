aa2394b3b7bf987475384c718f45de559f338892
jiyun.park <laurenceyoon@gmail.com>
2022-11-30 00:28:00 +0900

    update 221130

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 import asyncio
 from http import HTTPStatus
+import time
 
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket
 from fastapi.responses import RedirectResponse
 
 from .redis_client import redis_client
@@ -40,10 +41,12 @@ def redis_test(value: float = 1):
 @app.get("/current", tags=["Basic API"])
 def current_location():
     value = redis_client.get("location") or CURRENT_LOCATION
-    return {"location": value}
+    if int(value) == 228:
+        value = 0
+    return float(value)
 
 
-@app.patch(
+@app.get(
     "/follow",
     status_code=HTTPStatus.ACCEPTED,
     tags=["Basic API"],
@@ -53,3 +56,19 @@ def follow_piece(background_tasks: BackgroundTasks):
     
     background_tasks.add_task(start_performance_following, piece_path=piece)
     return {"response": f"following"}
+
+
+@app.websocket(
+    "/ws",
+)
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    print("websocket accept")
+    piece = "./resources/bwv66.6_piano.wav"
+    # asyncio.ensure_future(start_performance_following)
+    while True:
+        value = int(redis_client.get("location") or CURRENT_LOCATION)
+        print(f"value? {value}")
+        await websocket.send_text(f"{value}")
+        await asyncio.sleep(3)
+        print(f"sent!!")
